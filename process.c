6659b3265e7315106da6fcb352fc93e7a9c6fc27
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-04-27 01:00:57 +0200

    the process file is added

--- process.c
+++ process.c
@@ -1,17 +1,25 @@
-#include "headers.h"
+#include "Helpers.c"
 
 /* Modify this file as needed*/
-int remainingtime;
+int * remainingtime;
+int * shmid;
+int last=-1;
 
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    remainingtime=(int *)InitShm("m",shmid);
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
-    while (remainingtime > 0)
+    while (*remainingtime >= 0)
     {
-        // remainingtime = ??;
+        while (last==getClk());
+        if(last!=getClk())
+        {
+            *remainingtime--;
+            last=getClk();
+        }
+        
     }
     
     destroyClk(false);
