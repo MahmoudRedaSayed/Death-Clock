b8483e181a6176a05589b9de4be5e8d768e8c9f0
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-04-28 00:07:35 +0200

    the Queue is edited

--- Queue.c
+++ Queue.c
@@ -1,81 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"Helpers.c"
-#define QUEUE_TEMPLATE(ELTTYPE)  
  struct Node 
 { 
-        int capacity; 
-        int size; 
-        int front; 
-        int rear; 
-        ELTTYPE *elements; 
+        Node * Next;
+        Process data; 
 };
 typedef struct Node Node; 
  
-Node* createQueue(int maxElements) 
-{
-        /* Create a Queue */ 
-        Node *Q; 
-        Q = (Node*)malloc(sizeof(Node)); 
-        /* Initialise its properties */ 
-        Q->elements = malloc(sizeof(Node)*maxElements); 
-        Q->size = 0; 
-        Q->capacity = maxElements; 
-        Q->front = 0; 
-        Q->rear = -1; 
-        /* Return the pointer */ 
-        return Q; 
+Node* createQueue(Process Data) 
+{  
+        Node* temp = (Node*)malloc(sizeof(Node));
+        temp->data = Data;
+        temp->next = NULL;
+        return temp;
 } 
 void Dequeue(Node *Q) 
 { 
         /* If Queue size is zero then it is empty. So we cannot pop */ 
-        if(Q->size==0) 
+        if(Q==NULL) 
         { 
                 printf("Queue is Empty n"); 
                 return; 
         } 
-        /* Removing an element is equivalent to incrementing index of front by one */ 
         else 
         { 
-                Q->size--; 
-                Q->front++; 
-                /* As we fill elements in circular fashion */ 
-                if(Q->front==Q->capacity) 
-                { 
-                        Q->front=0; 
-                } 
+                Q=Q->Next;
         } 
         return; 
 } 
 Process front(Node*Q) 
 { 
-        if(Q->size==0) 
+        if(Q==NULL) 
         { 
                 printf("Queue is Empty n"); 
-                exit(0); 
+                return NULL;
         } 
         /* Return the element which is at the front*/ 
-        return Q->elements[Q->front]; 
+        return Q->data; 
 } 
-void Enqueue(Node*Q,ELTTYPE element) 
+void Enqueue(Node*Q,Process element) 
 { 
-        /* If the Queue is full, we cannot push an element into it as there is no space for it.*/ 
-        if(Q->size == Q->capacity) 
-        { 
-                printf("Queue is Full n"); 
-        } 
-        else 
-        { 
-                Q->size++; 
-                Q->rear++; 
-                /* As we fill the queue in circular fashion */ 
-                if(Q->rear == Q->capacity) 
-                { 
-                        Q->rear = 0; 
-                } 
-                /* Insert the element in its rear side */  
-                Q->elements[Q->rear] = element; 
-        } 
-        return; 
+        Node * temp=Q;
+        while(temp->Next==NULL)
+        {
+                temp=temp->Next;
+        }
+        temp->Next=(Node*)malloc(sizeof(Node));
+        temp->Next->data=element;
 }
 
